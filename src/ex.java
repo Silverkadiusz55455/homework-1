e2245e43452acff2ceb3675631c67a98592190c8
Silverkadiusz55455 <arkadiusz55455@gmail.com>
2021-07-26 20:23:23 +0200

    Commit #2

---
+++ src/ex.java
@@ -0,0 +1,11 @@
+public class ex {
+    public static void main(String[] args) {
+
+        //ex1
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("Programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
+    }
+}
